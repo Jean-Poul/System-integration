e3f8f0a766b23dca168b4a0f9a0e26732e636543
Jean-Poul <cph-jl360@cphbusiness.dk>
2022-09-07 16:04:01 +0200

    dev rework

--- assignment-2/src/main/java/dk/jplm/si/assignment2/Assignment2Application.java
+++ assignment-2/src/main/java/dk/jplm/si/assignment2/Assignment2Application.java
@@ -35,6 +35,9 @@ public class Assignment2Application implements CommandLineRunner {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        // need iP here - REWORK - LOOK AT DORAS CLIENT (DNS LOOKUP CANBRUGES SOM LOOKUP
+        // WWW.WHATISMYIP.COM/DNS-LOOKUP
+        // MAKE AN ENTITY
         String resourceURL = "https://api.genderize.io?name=peter&country_id=US";
         HttpEntity<String> entity = new HttpEntity<>(headers);
         ResponseEntity<String> response = restTemplate.exchange(resourceURL, HttpMethod.GET, entity, String.class);
