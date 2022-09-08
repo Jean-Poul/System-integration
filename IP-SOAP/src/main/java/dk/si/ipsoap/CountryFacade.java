602a347abf556bf2e0825d3482adf015ef9bbcd1
Mathias Parking <cph-mp525@cphbusiness.dk>
2022-09-08 08:57:50 +0200

    country code from ip

---
+++ IP-SOAP/src/main/java/dk/si/ipsoap/CountryFacade.java
@@ -0,0 +1,27 @@
+package dk.si.ipsoap;
+
+import javax.xml.rpc.ServiceException;
+import java.rmi.RemoteException;
+
+public class CountryFacade {
+    public static String getCountryFromIp(String IP) {
+        String country = "";
+        try
+        {
+            // Given an IP-address, this code finds the country from which it is located and returns "Country code: XX, state: XX"
+            GeoIPServiceLocator locator = new GeoIPServiceLocator();
+            GeoIPServiceSoap_PortType service = locator.getGeoIPServiceSoap();
+            String retString = service.getIpLocation(IP); // this ip is from dr.dk
+            retString = retString.replace("<GeoIP><Country>", "Country code: ");
+            retString = retString.replace("</Country><State>", ", state: ");
+            country = retString.replace("</State></GeoIP>", "");
+            // System.out.println("returned: " + country); for testing purposes
+        }
+        catch (ServiceException | RemoteException ex)
+        {
+            ex.printStackTrace();
+        }
+
+        return country;
+    }
+}
---
+++ IP-SOAP/src/test/java/dk/si/ipsoap/IpSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.si.ipsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpSoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
